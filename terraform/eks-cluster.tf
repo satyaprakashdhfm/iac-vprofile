cb1db41b800074bf7a5214905f99ba1c4d50c793
satyaprakashdhfm <satyaprakashreddy6789@gmail.com>
2025-08-28 20:16:55 +0530

    Rename EKS module arguments for v21.1.5 compatibility

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,12 +2,12 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.1.5"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.28"
+  name            = local.cluster_name
+  kubernetes_version = "1.28"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
